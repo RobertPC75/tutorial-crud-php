37b966c11124ea43d08b9523693a81feb0749875
RobertPC75 <98894368+RobertPC75@users.noreply.github.com>
2024-04-22 14:23:36 -0500

    testing

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ return [
     'host' => 'dashboard-crud-server.mysql.database.azure.com',
     'user' => 'pppnkxomil',
     'pass' => 'Robert75rpc!',
-    'name' => 'dashboard_crud_database',
+    'name' => 'test',
     'options' => [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
--- data/migracion.sql
+++ data/migracion.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE tutorial_crud;
+CREATE DATABASE test;
 
-use tutorial_crud;
+use test;
 
 CREATE TABLE trabajadores (
   id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
